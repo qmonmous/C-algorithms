7e7e5705a09cf1cdbe9ba5bcb0bcd32574708d1a
qmonmous <q.monmousseau@gmail.com>
2019-02-20 07:36:16 +0100

    little change

--- expressions-calculator/evalexpr.c
+++ expressions-calculator/evalexpr.c
@@ -76,11 +76,28 @@ int	eval_expr(char *expr)
 	return (parse_sum(&expr));
 }
 
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		putchar('-');
+		ft_putnbr(-nb);
+		return ;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		putchar(nb + '0');
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc > 1)
 	{
-		printf("%d", eval_expr(argv[1]));
+		ft_putnbr(eval_expr(argv[1]));
 		putchar('\n');
 	}
 	return (0);
